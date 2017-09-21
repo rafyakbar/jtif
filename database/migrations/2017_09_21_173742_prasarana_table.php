861346cc3075cd2dc53273e4334fb195551e4e8c
Rafy Aulia Akbar <rafy683@gmail.com>
2017-09-21 18:04:57 +0700

    penambahan tabel prasarana

---
+++ database/migrations/2017_09_21_173742_prasarana_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class PrasaranaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prasarana', function (Blueprint $table){
+            $table->increments('id');
+            $table->string('nama')->unique();
+            $table->text('keterangan');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return voidSword Art Online Movie Ordinal Scale
+     */
+    public function down()
+    {
+        //
+    }
+}
