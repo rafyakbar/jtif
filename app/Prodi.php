f63315ddf868df98e72755d9a6dd0a4ca53a4cb3
Rafy Aulia Akbar <rafy683@gmail.com>
2017-09-21 22:59:56 +0700

    penambahan model prodi + seedernya

---
+++ app/Prodi.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Prodi extends Model
+{
+    protected $table = 'prodi';
+
+    protected $fillable = [
+        'nama', 'keterangan'
+    ];
+
+    public function pegawai()
+    {
+        $this->hasMany('App\Pegawai', 'prodi_id');
+    }
+}
---
+++ database/seeds/ProdiSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ProdiSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    const TI = [
+        'D III Manajemen Informatika',
+        'S1 Teknik Informatika',
+        'S1 Sistem Informasi',
+        'S1 Pendidikan Teknologi Informasi'
+    ];
+
+    public function run()
+    {
+        foreach (static::TI as $value){
+            Prodi::create([
+                'nama' => $value
+            ]);
+        }
+    }
+}
