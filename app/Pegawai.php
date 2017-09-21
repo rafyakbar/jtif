db3330d7e644ade41ce12652793ca4ab954815a4
Rafy Aulia Akbar <rafy683@gmail.com>
2017-09-21 23:24:03 +0700

    penambahan model pegawai + seedernya

--- app/Pegawai.php
+++ app/Pegawai.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pegawai extends Model
 {
+    const JABATAN = [
+        'Ketua Jurusan',
+        'Sekretaris Jurusan',
+        'Pembina Kemahasiswaan'
+    ];
+
     protected $table = 'pegawai';
 
     protected $timestamps = false;
