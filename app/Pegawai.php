2edb73d70beb0b42c1545aef8bc8670d9994e5f7
Unknown <mamahit61@gmail.com>
2017-09-25 15:17:30 +0700

    membuat seeder pegawai

--- app/Pegawai.php
+++ app/Pegawai.php
@@ -7,20 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 class Pegawai extends Model
 {
     const JABATAN = [
-        'Ketua Jurusan',
-        'Sekretaris Jurusan',
-        'Pembina Kemahasiswaan',
-        'Kaprodi D3 Manajemen Informatika',
-        'Kaprodi S1 Pendidikan Teknologi Informasi',
-        'Kaprodi S1 Sistem Informasi',
-        'Kaprodi S1 Teknik Informatika',
-        'Unit Penunjang',
-        'Ketua Lab',
-        'Ketua Lab Rekayasa Perangkat Lunak',
-        'Ketua Lab Multimedia Kreatif',
-        'Ketua Lab Sistem Informasi',
-        'Ketua Lab Jaringan',
-        'Staf Pelaksana'
+        'Dekan', //0
+        'Ketua Jurusan', //1
+        'Sekretaris Jurusan',//2
+        'Pembina Kemahasiswaan', //3
+        'Kaprodi D3 Manajemen Informatika', //4
+        'Kaprodi S1 Pendidikan Teknologi Informasi', //5
+        'Kaprodi S1 Sistem Informasi',//6
+        'Kaprodi S1 Teknik Informatika',//7
+        'Unit Penunjang',//8
+        'Ketua Lab',//9
+        'Ketua Lab Rekayasa Perangkat Lunak',//10
+        'Ketua Lab Multimedia Kreatif',//11
+        'Ketua Lab Sistem Informasi',//12
+        'Ketua Lab Jaringan',//13
+        'Staf Pelaksana'//14
     ];
 
     protected $table = 'pegawai';
--- database/seeds/PegawaiSeeder.php
+++ database/seeds/PegawaiSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Pegawai;
 
 class PegawaiSeeder extends Seeder
 {
@@ -11,6 +12,209 @@ class PegawaiSeeder extends Seeder
      */
     public function run()
     {
-
+        Pegawai::create([
+            'id' => '196004041987011001',
+            'prodi_id' => 2,
+            'nama' => 'Prof. Dr. Ekohariadi, M.Pd',
+            'jenis_kelamin' => 'Pria',
+            'jabatan' => Pegawai::JABATAN[0],
+        ]);
+        Pegawai::create([
+            'id' => '196505191992021001',
+            'prodi_id' => 1,
+            'nama' => 'Drs. BAMBANG SUJATMIKO, M.T.',
+            'jenis_kelamin' => 'Pria',
+            'jabatan' => Pegawai::JABATAN[8],
+        ]);
+        Pegawai::create([
+            'id' => '196812171994032002',
+            'prodi_id' => 1,
+            'nama' => 'Rina Harimurti, S.Pd.,MT',
+            'jenis_kelamin' => 'Wanita',
+            'jabatan' => Pegawai::JABATAN[1],
+        ]);
+        Pegawai::create([
+            'id' => '196105151986012001',
+            'prodi_id' => 2,
+            'nama' => 'Dr. MEINI SONDANG SUMBAWATI, M.Pd.',
+            'jenis_kelamin' => 'Wanita',
+            'jabatan' => Pegawai::JABATAN[8],
+        ]);
+        Pegawai::create([
+            'id' => '196901251995122001',
+            'prodi_id' => 1,
+            'nama' => 'Anita Qoiriah, S.Kom., M.Kom',
+            'jenis_kelamin' => 'Wanita',
+            'jabatan' => Pegawai::JABATAN[2],
+        ]);
+        Pegawai::create([
+            'id' => '197411012003121001',
+            'prodi_id' => 4,
+            'nama' => 'Aditya Prapanca, ST. M.Kom',
+            'jenis_kelamin' => 'Pria',
+            'jabatan' => Pegawai::JABATAN[7],
+        ]);
+        Pegawai::create([
+            'id' => '197702032005012001',
+            'prodi_id' => 1,
+            'nama' => 'Wiyli Yustanti, S.Si. M.Kom',
+            'jenis_kelamin' => 'Wanita',
+            'jabatan' => Pegawai::JABATAN[8],
+        ]);
+        Pegawai::create([
+            'id' => '197512032005012001',
+            'prodi_id' => 1,
+            'nama' => 'NAIM ROCHMAWATI ,S.Kom. MT.',
+            'jenis_kelamin' => 'Wanita',
+            'jabatan' => Pegawai::JABATAN[9],
+        ]);
+        Pegawai::create([
+            'id' => '197908062006041001',
+            'prodi_id' => 4,
+            'nama' => 'Agus Prihanto, ST. M.Kom',
+            'jenis_kelamin' => 'Pria',
+            'jabatan' => Pegawai::JABATAN[8],
+        ]);
+        Pegawai::create([
+            'id' => '197506022003122001',
+            'prodi_id' => 4,
+            'nama' => 'Yuni Yamasari, S.Kom., M.Kom',
+            'jenis_kelamin' => 'Wanita',
+            'jabatan' => Pegawai::JABATAN[8],
+        ]);
+        Pegawai::create([
+        'id' => '198004122006042002',
+        'prodi_id' => 3,
+        'nama' => 'ARIES DWI INDRIYANTI, S.Kom.,M.Kom.',
+        'jenis_kelamin' => 'Wanita',
+        'jabatan' => Pegawai::JABATAN[8],
+         ]);
+        Pegawai::create([
+            'id' => '197303302006041001',
+            'prodi_id' => 3,
+            'nama' => 'Ari Kurniawan, S.Kom.,MT',
+            'jenis_kelamin' => 'Pria',
+            'jabatan' => Pegawai::JABATAN[10],
+        ]);
+        Pegawai::create([
+            'id' => '197912062008011011',
+            'prodi_id' => 3,
+            'nama' => 'Dedy Rahman Prehanto. S.Kom M.Kom',
+            'jenis_kelamin' => 'Pria',
+            'jabatan' => Pegawai::JABATAN[10],
+        ]);
+        Pegawai::create([
+            'id' => '197810272008121002',
+            'prodi_id' => 1,
+            'nama' => 'ANDI IWAN NURHIDAYAT, S.Kom.,M.T.',
+            'jenis_kelamin' => 'Pria',
+            'jabatan' => Pegawai::JABATAN[8],
+        ]);
+        Pegawai::create([
+            'id' => '198211022008121001',
+            'prodi_id' => 4,
+            'nama' => 'SALAMUN ROHMAN NUDIN, S.Kom.,M.Kom.',
+            'jenis_kelamin' => 'Pria',
+            'jabatan' => Pegawai::JABATAN[8],
+        ]);
+        Pegawai::create([
+            'id' => '197701102008121003',
+            'prodi_id' => 1,
+            'nama' => 'ASMUNIN , S.Kom.',
+            'jenis_kelamin' => 'Pria',
+            'jabatan' => Pegawai::JABATAN[4],
+        ]);
+        Pegawai::create([
+            'id' => '198003252008121001',
+            'prodi_id' => 4,
+            'nama' => 'I GUSTI LANANG P.E.P., S.Kom., M.Kom.',
+            'jenis_kelamin' => 'Pria',
+            'jabatan' => Pegawai::JABATAN[13],
+        ]);
+        Pegawai::create([
+            'id' => '197912202008121001',
+            'prodi_id' => 3,
+            'nama' => 'DWI FATRIANTO SUYATNO ,S.Kom.,M.Kom.',
+            'jenis_kelamin' => 'Pria',
+            'jabatan' => Pegawai::JABATAN[6],
+        ]);
+        Pegawai::create([
+            'id' => '198102212008122001',
+            'prodi_id' => 3,
+            'nama' => 'ARDHINI WARIH UTAMI, S.Kom., M.Kom',
+            'jenis_kelamin' => 'Wanita',
+            'jabatan' => Pegawai::JABATAN[8],
+        ]);
+        Pegawai::create([
+            'id' => '197905082009121001',
+            'prodi_id' => 2,
+            'nama' => 'Setya Chendra Wibawa, S.Pd, MT',
+            'jenis_kelamin' => 'Pria',
+            'jabatan' => Pegawai::JABATAN[5],
+        ]);
+        Pegawai::create([
+            'id' => '1112',
+            'prodi_id' => 2,
+            'nama' => 'Yeni Anistyasari, S.Pd. M.Kom',
+            'jenis_kelamin' => 'Wanita',
+            'jabatan' => Pegawai::JABATAN[8],
+        ]);
+        Pegawai::create([
+        'id' => '198104142009121004',
+        'prodi_id' => 3,
+        'nama' => 'I KADEK DWI NURYANA ,S.T., M.Kom.',
+        'jenis_kelamin' => 'Pria',
+        'jabatan' => Pegawai::JABATAN[8],
+        ]);
+        Pegawai::create([
+            'id' => '0716018704',
+            'prodi_id' => 4,
+            'nama' => 'Ricky Eka Putra, S.Kom. M.Kom',
+            'jenis_kelamin' => 'Pria',
+            'jabatan' => Pegawai::JABATAN[8],
+        ]);
+        Pegawai::create([
+            'id' => '1111',
+            'prodi_id' => 1,
+            'nama' => 'Ibnu Febry Kurniawan, S.Kom, M.Sc',
+            'jenis_kelamin' => 'Pria',
+            'jabatan' => Pegawai::JABATAN[11],
+        ]);
+        Pegawai::create([
+            'id' => '1113',
+            'nama' => 'Wuri Handayani, S.H.',
+            'jenis_kelamin' => 'Wanita',
+            'jabatan' => Pegawai::JABATAN[14],
+        ]);
+        Pegawai::create([
+            'id' => '1114',
+            'nama' => 'Ratna Prahastuti',
+            'jenis_kelamin' => 'Wanita',
+            'jabatan' => Pegawai::JABATAN[14],
+        ]);
+        Pegawai::create([
+            'id' => '1115',
+            'nama' => 'Benny Yulianto',
+            'jenis_kelamin' => 'Pria',
+            'jabatan' => Pegawai::JABATAN[14],
+        ]);
+        Pegawai::create([
+            'id' => '1116',
+            'nama' => 'Dodik Arwin D., SST,MT',
+            'jenis_kelamin' => 'Pria',
+            'jabatan' => Pegawai::JABATAN[14],
+        ]);
+        Pegawai::create([
+            'id' => '1117',
+            'nama' => 'Sholikhun, ST.',
+            'jenis_kelamin' => 'Pria',
+            'jabatan' => Pegawai::JABATAN[14],
+        ]);
+        Pegawai::create([
+            'id' => '1118',
+            'nama' => 'Sugianto, AMd.',
+            'jenis_kelamin' => 'Pria',
+            'jabatan' => Pegawai::JABATAN[14],
+        ]);
     }
 }
